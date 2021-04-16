2d1fa76cbb83d18ce2ed54417173e54ec265c6bc
unknown <vandung030701@gmail.com>
2021-04-16 18:50:45 +0700

    W_BAND

---
+++ w3_band/about.js
@@ -0,0 +1,9 @@
+setInterval(function() {
+    document.getElementsByClassName('banner-1').style.display = none;
+}, 3000);
+setInterval(function() {
+    document.getElementsByClassName('banner-2').style.display = block;
+}, 3000);
+setInterval(function() {
+    document.getElementsByClassName('banner-3').style.display = block;
+}, 7000);
